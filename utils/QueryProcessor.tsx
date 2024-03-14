9db4657a2ab1282f2282440882ccfa3140663e82
Deon Kouatchou <ddkouatchou@gmail.com>
2024-03-14 15:51:56 -0400

    Modified name query response

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
-  else if (query.toLowerCase().includes("what is your name?")) {
+  else if (query.toLowerCase().includes("name?")) {
     return (
       "My name is Git Goon 1. Why am I Git Goon 1? Because I am the greatest. " +
       "The greatest Git Goon there ever was, there is, and there will ever be. " +
